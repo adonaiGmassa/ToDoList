0c03132dd113b859af803ebf122b1ef1cb8eb367
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2025-01-20 21:52:27 +0100

    Création form nouvelleTache

---
+++ ToDoList/NouvelleTache.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ToDoList
+{
+    partial class NouvelleTache
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValider = new System.Windows.Forms.Button();
+            this.txtTache = new System.Windows.Forms.TextBox();
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.btnAnnule = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(201, 381);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(143, 31);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // txtTache
+            // 
+            this.txtTache.Location = new System.Drawing.Point(213, 100);
+            this.txtTache.Name = "txtTache";
+            this.txtTache.Size = new System.Drawing.Size(374, 26);
+            this.txtTache.TabIndex = 3;
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Arial Black", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(99, 9);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(596, 56);
+            this.lblTitre.TabIndex = 5;
+            this.lblTitre.Text = "Ajoute une nouvelle tache";
+            // 
+            // btnAnnule
+            // 
+            this.btnAnnule.Location = new System.Drawing.Point(477, 381);
+            this.btnAnnule.Name = "btnAnnule";
+            this.btnAnnule.Size = new System.Drawing.Size(143, 31);
+            this.btnAnnule.TabIndex = 6;
+            this.btnAnnule.Text = "Annule";
+            this.btnAnnule.UseVisualStyleBackColor = true;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(213, 158);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(374, 118);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(254, 295);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 26);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // NouvelleTache
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnAnnule);
+            this.Controls.Add(this.lblTitre);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.txtTache);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NouvelleTache";
+            this.Text = "NouvelleTache";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.TextBox txtTache;
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Button btnAnnule;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
